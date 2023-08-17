c2046c154641767bdc5507df94dba94ffb14b795
Charita <charita.dec@gmail.com>
2023-08-17 17:09:19 +0530

    Created essential feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Charita R on 17/08/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
